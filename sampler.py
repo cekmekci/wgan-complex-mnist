fixed the gradient of log-posterior

sampler.py
@@ -26,15 +26,19 @@ class MALA_Poisson_Sampler():
     def log_p_z_given_y(self, z):
         # ignoring logp(y)
         AGz = self.A(self.generator(z))
-        log_likelihood = torch.sum(self.measurement * torch.log(torch.sum(AGz**2,2,keepdim=True) + self.eps) - torch.sum(AGz**2,2,keepdim=True))
+        AGz_squared_sum = torch.sum(AGz**2, 2, keepdim=True)
+        log_likelihood = torch.sum(self.measurement * torch.log(AGz_squared_sum + self.eps) - AGz_squared_sum)
         log_prior = -1 / 2 * torch.sum(z**2)
         return log_likelihood + log_prior
 
     def grad_log_p_z_given_y(self, z):
+        # gradient of log_p_y_given_z
         AGz = self.A(self.generator(z)) # (1,S,2,H2,W2)
         vec = self.AT(AGz * (self.measurement / (torch.sum(AGz**2,2,keepdim=True) + self.eps)  - 1)) # (1,2,64,64)
         _, vjp = torch.autograd.functional.vjp(self.generator, z, v=vec, create_graph=False, strict=True) # (1, latent_dim)
         grad = 2 * vjp
+        # add the gradient of log_p_z, which is -z
+        grad = grad - z
         return grad
 
     def log_q_zbar_given_ztilde(self, z_bar, z_tilde):
@@ -113,3 +117,95 @@ def optimize_latent_variable(G, x, z_dim, lr=1e-4, num_steps=1000, verbose=False
         if verbose and (step % 100 == 0):
             print(f'Step {step}/{num_steps}, Loss: {loss.item()}')
     return z.detach()
+
+
+# Numerically test if the gradient calculation is correct
+if __name__ == '__main__':
+
+    from utils_ptycho import ptycho_forward_op, ptycho_adjoint_op, cartesian_scan_pattern
+    from models import Generator
+    import matplotlib.pyplot as plt
+
+    object_size = (64,64)
+    probe_size = (1,2,16,16)
+    latent_dim = 100
+
+    complex_object = torch.randn(1,2,*object_size)
+    scan = cartesian_scan_pattern(object_size, probe_size, step_size = 4, sigma = 0.1)
+    probe = torch.randn(probe_size)
+
+    A = lambda x: ptycho_forward_op(x, scan, probe)
+    AH = lambda x: ptycho_adjoint_op(x, scan, probe, object_size)
+
+    measurement = A(complex_object)
+    measurement = torch.sum(measurement**2, 2, keepdim=True) # (1,S,1,H2,W2)
+    measurement = torch.poisson(measurement)
+
+    z = torch.randn((1,latent_dim))
+
+    generator = Generator(img_size=(2, object_size[0], object_size[1]), latent_dim=latent_dim, dim=128)
+    generator.eval()
+
+    sampler = MALA_Poisson_Sampler(A, AH, generator, measurement, z,
+                                    num_iter=10000, step_size=1e-5, burn_in=500,
+                                    use_cuda=False)
+
+    # Our closed form gradient
+    grad = sampler.grad_log_p_z_given_y(z)
+
+    errors = []
+    eps_list = np.logspace(-3, 2, 25)
+    for eps in eps_list:
+        # Numerical gradient
+        numerical_grad = torch.zeros_like(grad)
+        for i in range(latent_dim):
+            e_i = torch.zeros_like(z)
+            e_i[:,i] = 1.0
+            z_plus = z + eps * e_i
+            z_minus = z - eps * e_i
+            num_grad = (sampler.log_p_z_given_y(z_plus) - sampler.log_p_z_given_y(z_minus)) / (2*eps)
+            numerical_grad[:,i] = num_grad
+        # Check the difference
+        error = torch.mean(torch.abs(grad - numerical_grad))
+        errors.append(error)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(eps_list, errors, marker='o', color='white', linestyle='-', linewidth=2)
+    plt.xscale('log')
+    plt.yscale('log')
+
+    # Set plot style
+    ax = plt.gca()
+    fig = plt.gcf()
+
+    ax.set_facecolor('black')
+    fig.patch.set_facecolor('black')  # Set the figure background to black
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+    ax.yaxis.label.set_color('white')
+    ax.xaxis.label.set_color('white')
+    ax.title.set_color('white')
+
+    # Set grid style
+    plt.grid(color='white', linestyle='--', linewidth=0.5)
+
+    # Set axis color
+    ax.spines['bottom'].set_color('white')
+    ax.spines['left'].set_color('white')
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+
+    # Also, set the color of the ticks and labels
+    ax.xaxis.label.set_color('white')
+    ax.yaxis.label.set_color('white')
+    ax.title.set_color('white')
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+
+    # Set labels and title
+    plt.xlabel('Eps Values')
+    plt.ylabel('Gradient Error')
+    plt.title('Gradient Error vs. Perturbation Parameter (Eps)')
+
+    # Show plot
+    plt.savefig("gradient_errors.png")